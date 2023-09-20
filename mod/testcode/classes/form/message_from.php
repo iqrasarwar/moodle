996ed6082353a32f9d0a8bbfa080508087c9527b
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2023-09-20 10:52:17 +0500

    testcode acitivity module added

---
+++ mod/testcode/classes/form/message_from.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class message_from
+ *
+ * @package    mod_testcode
+ * @copyright  2023 true
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once($CFG->libdir . '/formslib.php');
+
+defined('MOODLE_INTERNAL') || die();
+
+
+class message_form extends moodleform
+{
+    /**
+     * Define the form.
+     */
+    public function definition()
+    {
+        $mform = $this->_form;
+
+        $mform->addElement('textarea', 'message', get_string('yourmessage', 'mod_testcode'));
+        $mform->setType('message', PARAM_TEXT);
+
+        $id = optional_param('id', 0, PARAM_INT);
+
+        if ($id) {
+            $cm = get_coursemodule_from_id('testcode', $id, 0, false, MUST_EXIST);
+        }
+
+        $mform->addElement('hidden', 'id', $cm->id);
+        $mform->setType('id', PARAM_INT); // Set type of element.
+
+        $submitlabel = get_string('submit');
+        $mform->addElement('submit', 'submitmessage', $submitlabel);
+    }
+}
---
+++ mod/testcode/lang/en/testcode.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_testcode
+ * @category    string
+ * @copyright   2023 iqra <iqra.sarwar@arbisoft.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Test Code';
+$string['modulenameplural'] = 'Test Codes';
+$string['modulename'] = 'Test Code';
+$string['testcodename'] = 'Test Code';
+$string['testcodename_help'] = 'Test Code Help';
+$string['testcode:addinstance'] = 'Add a test code instance';
+$string['testcode:view'] = 'View a test code instance';
+$string['testcode:postmessages'] = 'Post Messages';
+$string['testcode:viewmessages'] = 'View Messages';
+$string['no$testcodeinstances'] = "No Instance present";
+$string['yourmessage'] = "Message container ->";
+$string['testcodefieldset'] = "Custom Message";
